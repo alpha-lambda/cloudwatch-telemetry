48f9382d6a834edcc6ef685f2c206a4aa440e966
AntonBazhal <am.bazhal@gmail.com>
2023-05-01 11:36:37 -0400

    Remove yn

--- src/conf.js
+++ src/conf.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const yn = require('yn');
-
 module.exports = {
   cloudwatch: {},
   logLevel: process.env.LOG_LEVEL || 'error',
-  suppressErrors: yn(process.env.SUPPRESS_ERRORS, { default: false })
+  suppressErrors: process.env.SUPPRESS_ERRORS === 'true',
 };
